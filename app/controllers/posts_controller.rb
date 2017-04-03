426a8a1daefeeb21bc7afd09427df2b5ed4ba908
Zakai <Zakai@ZakaiBook.local>
2017-04-03 13:52:08 -0700

    add post creation and viewing functionality

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,35 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    @user = current_user
+  end
+
+  def new
+    @user = current_user
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @author = User.find(@post.user_id)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text, :user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  root 'welcome#index'
 
   devise_for :users
+
+  resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
