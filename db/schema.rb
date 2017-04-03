97caaf96fd84e34ecf963f10f2607834703eedfa
Zakai <Zakai@ZakaiBook.local>
2017-04-03 14:51:56 -0700

    add comments to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403205339) do
+ActiveRecord::Schema.define(version: 20170403214642) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "text"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
