91cadab8319385611352c26b5db4105be3027d0f
Zakai <Zakai@ZakaiBook.local>
2017-04-04 12:23:58 -0700

    add author name display logic to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :post
+
+	def print_author_name
+		name_string = self.user.first_name + ' ' + self.user.last_name
+		p name_string
+	end
 end
