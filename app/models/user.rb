9094471e379809c3d36aa596e56b15373c91ba6a
Zakai <Zakai@ZakaiBook.local>
2017-04-05 14:01:26 -0700

    remove confirmable from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
   validates :first_name, :last_name, presence: true
   has_many :posts
   has_many :comments
--- db/schema.rb
+++ db/schema.rb
@@ -42,14 +42,10 @@ ActiveRecord::Schema.define(version: 20170403214642) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
     t.string   "first_name"
     t.string   "last_name"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
